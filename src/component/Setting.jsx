82baeae41b84a568eeb2884cbcf5bf4685c3f62c
Kingslet Effiong <kingsleyeffiong642@gmail.com>
2024-10-25 08:45:12 +0100

    updated

--- src/component/Setting.jsx
+++ src/component/Setting.jsx
@@ -5,12 +5,13 @@ function Setting() {
     const navigate = useNavigate();
     return (
         <div className={styles.container}>
-            <h1 style={{color:'var(--color-white--1)', margin:'3rem 0'}}>Setting</h1>
+            <h1 style={{color:'var(--color-white--1)', margin:'3rem 0 0 0'}}>Welcome back!</h1>
+            <h3 style={{color:'var(--color-white--1)'}}>Add your first transaction</h3>
             <div className={styles.Ellipse12}></div>
             <div className={styles.Ellipse13}></div>
             <div className={styles.Ellipse14}></div>
             <div className={styles.contents}>
-                <Button className={styles.button} onClick={()=>{}}>Update Information</Button>
+                {/* <Button className={styles.button} onClick={()=>{}}>Update Information</Button> */}
                 <Button className={styles.button}  onClick={() => navigate('/form-income')}>Add income</Button>
                 <Button className={styles.button}  onClick={() => navigate('/form-expense')}>Add Expense</Button>
             </div>
